acff8a71fe89e20e2de14aafcc635b27a57c9841
Lyubov Protasova <lyubovap@gmail.com>
2022-10-05 17:54:21 -0400

    test

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,88 +1,70 @@
 package ui;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
+
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import algonquin.cst2335.prot0003.R;
 import algonquin.cst2335.prot0003.databinding.ActivityMainBinding;
 import data.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding variableBinding;
-    private MainViewModel model;
-
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//       connectAppCompatActivity to ViewModel class;
-        model = new ViewModelProvider(this).get(MainViewModel.class);
+        setContentView(R.layout.activity_main);
 
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());//loads XML
-        TextView myText = variableBinding.textview;
-        CheckBox checkBox = variableBinding.checkBox;
-        Switch switch1 = variableBinding.switch1;
-        RadioButton radioButton = variableBinding.radioButton;
-        ImageButton myimagebutton = variableBinding.myimagebutton;
-        EditText myedit = variableBinding.myedittext;
+        imgView = findViewById(R.id.flagView);
+        sw = findViewById(R.id.switch1);
 
-        variableBinding.mybutton.setOnClickListener(click ->
-        {
-            String editString = myedit.getText().toString();
 
-        model.editString.postValue(editString);
-        });
 
-        //call the observe() function, Lambda() function
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
 
-        model.editString.observe( this, s -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-            variableBinding.textview.setText("Your edit text has" + s);
-        });
-        //selected button
-
-        model.isSelected.observe( this, selected -> {
-
-//            int x = 1;
-            variableBinding.checkBox.setChecked(selected);
-            variableBinding.radioButton.setChecked(selected);
-            variableBinding.switch1.setChecked(selected);
-        Toast.makeText( this, "The value now is: " + selected, Toast.LENGTH_LONG).show();
-        });
-
-        variableBinding.checkBox.setOnCheckedChangeListener( (button, isSelected) -> {
-                    model.isSelected.postValue(isSelected);
-                });
-
-        variableBinding.radioButton.setOnCheckedChangeListener( (button, isSelected) -> {
-                            model.isSelected.postValue(isSelected);
-                });
-
-        variableBinding.switch1.setOnCheckedChangeListener( (button, isSelected) -> {
-                        model.isSelected.postValue(isSelected);
-                    });
-        //ImageButton
-        myimagebutton.setOnClickListener(click -> {
-            Toast.makeText(this, "the width =" +variableBinding.myimagebutton.getWidth() + " the heigth =" +variableBinding.myimagebutton.getHeight(), Toast.LENGTH_LONG).show();
-
-    });
+                imgView.startAnimation(rotate);
+            }else {
+                imgView.clearAnimation();
             }
+
+        });
+
+//        sw = setOnCheckedChangeListener((btn, isChecked)) -> {
+//
+//            if (isChecked)
+//            {
+//                RotateAnimation rotate = new RotateAnimation (0, 360,
+//                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//                rotate.set.Duration (5000);
+//                rotate.setRepeatCount(Animation.INFINITE);
+//                rotate.setInterpolator(new LenearInterpolator());
+//               imgView.startAnimation(rotate);
+//            }
+//            else {
+//                imgView.clearAnimation();
+//            }
+//        });
         }
+    }
+
+
 
 
 
